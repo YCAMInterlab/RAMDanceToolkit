44f2539fe98c9ae500189626c961256a9aefb4bb
Kyle McDonald <kyle@kylemcdonald.net>
2013-01-11 21:37:47 +0900

    drawing skeletons, next is calibrating the kinects to each other. sometimes when openni drops the skeleton it has a really hard time coming back, might need to add a reset to the sender app.

--- dev/OpenNIMerge/src/ofApp.cpp
+++ dev/OpenNIMerge/src/ofApp.cpp
@@ -17,7 +17,7 @@ string formatPosition(ofVec3f point) {
 void ofApp::setup() {
 	ofSetVerticalSync(true);
 	ofSetFrameRate(120);
-
+	
 	ofxXmlSettings settings;
 	settings.loadFile("settings.xml");
 	
@@ -32,16 +32,16 @@ void ofApp::update() {
 		osc.getNextMessage(&msg);
 		if(checkAddress(msg, "openni", 0)) {
 			int deviceID = ofToInt(getAddress(msg, 1));
-			OpenNI& openni = opennis[deviceID];
+			OscOpenNI& openni = opennis[deviceID];
 			if(checkAddress(msg, "user", 2)) {
 				int xnId = ofToInt(getAddress(msg, 3));
-				User& user = openni.users[xnId];
+				OscUser& user = openni.users[xnId];
 				if(checkAddress(msg, "joints", 4)) {
-					const int elementsPerJoint = 4;
-					int numJoints = msg.getNumArgs() / elementsPerJoint;
+					const int elementsPerOscJoint = 4;
+					int numOscJoints = msg.getNumArgs() / elementsPerOscJoint;
 					int j = 0;
-					for(int i = 0; i < numJoints; i++) {
-						Joint& joint = user.joints[i];
+					for(int i = 0; i < numOscJoints; i++) {
+						OscJoint& joint = user.joints[i];
 						joint.position.x = msg.getArgAsFloat(j++);
 						joint.position.y = msg.getArgAsFloat(j++);
 						joint.position.z = msg.getArgAsFloat(j++);
@@ -63,15 +63,18 @@ void ofApp::draw() {
 	cam.begin();
 	ofScale(.05, .05, .05);
 	ofDrawGrid(2000, 10, false, true, true, true);
-	for(map<int, OpenNI>::iterator openniItr = opennis.begin(); openniItr != opennis.end(); openniItr++) {
-		OpenNI& openni = openniItr->second;
-		for(map<int, User>::iterator userItr = openni.users.begin(); userItr != openni.users.end(); userItr++) {
-			User& user = userItr->second;
-			for(map<int, Joint>::iterator jointItr = user.joints.begin(); jointItr != user.joints.end(); jointItr++) {
-				Joint& joint = jointItr->second;
+	for(map<int, OscOpenNI>::iterator openniItr = opennis.begin(); openniItr != opennis.end(); openniItr++) {
+		OscOpenNI& openni = openniItr->second;
+		for(map<int, OscUser>::iterator userItr = openni.users.begin(); userItr != openni.users.end(); userItr++) {
+			OscUser& user = userItr->second;
+			ofSetColor(255);
+			user.drawSkeleton();
+			for(map<int, OscJoint>::iterator jointItr = user.joints.begin(); jointItr != user.joints.end(); jointItr++) {
+				OscJoint& joint = jointItr->second;
 				ofSetColor(ofColor::fromHsb(255 * joint.confidence, 255, 255));
 				ofBox(joint.position, 10);
-				ofDrawBitmapString(formatPosition(join.position), joint.position);
+				ofSetColor(255);
+				//ofDrawBitmapString(formatPosition(joint.position), joint.position);
 			}
 		}
 	}
--- dev/OpenNIMerge/src/ofApp.h
+++ dev/OpenNIMerge/src/ofApp.h
@@ -4,32 +4,87 @@
 #include "ofxOsc.h"
 #include "ofxXmlSettings.h"
 
-class Joint {
+enum Joint {
+    // start at root joint
+    JOINT_TORSO = 0,
+    JOINT_NECK,
+    JOINT_HEAD,
+
+    // left arm + shoulder
+    JOINT_LEFT_SHOULDER,
+    JOINT_LEFT_ELBOW,
+    JOINT_LEFT_HAND,
+
+    // right arm + shoulder
+    JOINT_RIGHT_SHOULDER,
+    JOINT_RIGHT_ELBOW,
+    JOINT_RIGHT_HAND,
+
+    // left leg
+    JOINT_LEFT_HIP,
+    JOINT_LEFT_KNEE,
+    JOINT_LEFT_FOOT,
+
+    // right leg
+    JOINT_RIGHT_HIP,
+    JOINT_RIGHT_KNEE,
+    JOINT_RIGHT_FOOT,
+
+    JOINT_COUNT,
+    JOINT_UNKOWN
+};
+
+const static int skeletonLimbSize = 14;
+const static int skeletonLimbs[] = {
+	JOINT_HEAD, JOINT_NECK,
+	JOINT_NECK, JOINT_TORSO,
+	JOINT_RIGHT_SHOULDER, JOINT_NECK,
+	JOINT_RIGHT_ELBOW, JOINT_RIGHT_SHOULDER,
+	JOINT_RIGHT_HAND, JOINT_RIGHT_ELBOW,
+	JOINT_LEFT_SHOULDER, JOINT_NECK,
+	JOINT_LEFT_ELBOW, JOINT_LEFT_SHOULDER,
+	JOINT_LEFT_HAND, JOINT_LEFT_ELBOW,
+	JOINT_LEFT_HIP, JOINT_TORSO,
+	JOINT_LEFT_KNEE, JOINT_LEFT_HIP,
+	JOINT_LEFT_FOOT, JOINT_LEFT_KNEE,
+	JOINT_RIGHT_HIP, JOINT_TORSO,
+	JOINT_RIGHT_KNEE, JOINT_RIGHT_HIP,
+	JOINT_RIGHT_FOOT, JOINT_RIGHT_KNEE,
+};
+
+class OscJoint {
 public:
 	ofVec3f position;
 	float confidence;
 	
-	Joint()
+	OscJoint()
 	:confidence(0) {
 	}
 };
 
-class User {
+class OscUser {
 public:
 	bool isFound, isTracking, isSkeleton, isCalibrating;
-	map<int, Joint> joints;
+	map<int, OscJoint> joints;
 	
-	User()
+	OscUser()
 	:isFound(false)
 	,isTracking(false)
 	,isSkeleton(false)
 	,isCalibrating(false) {
 	}
+	void drawSkeleton() {
+		for(int i = 0; i < skeletonLimbSize; i++) {
+			int root = skeletonLimbs[i * 2];
+			int parent = skeletonLimbs[i * 2 + 1];
+			ofLine(joints[root].position, joints[parent].position);
+		}
+	}
 };
 
-class OpenNI {
+class OscOpenNI {
 public:
-	map<int, User> users;
+	map<int, OscUser> users;
 };
 
 class ofApp : public ofBaseApp {
@@ -39,6 +94,6 @@ public:
 	void draw();
 	
 	ofxOscReceiver osc;
-	map<int, OpenNI> opennis;
+	map<int, OscOpenNI> opennis;
 	ofEasyCam cam;
 };
