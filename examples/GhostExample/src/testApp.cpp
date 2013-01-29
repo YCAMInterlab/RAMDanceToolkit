11f684f1bab4763abca5a5f8bce0e842c8660074
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-01-29 23:41:42 +0900

    added example/GhostExample

---
+++ examples/GhostExample/src/testApp.cpp
@@ -0,0 +1,146 @@
+#include "testApp.h"
+
+
+static const string myActorName = "default_unknown_date";
+ramGhost ghost;
+
+
+#pragma mark - oF methods
+//--------------------------------------------------------------
+void testApp::setup()
+{
+	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofBackground( ramColor::WHITE );
+	
+	
+	/*!
+	 ramBaseApp setup
+	 */
+	ramEnableAllEvents();
+	oscReceiver.setup(10000);
+	
+	
+	/*!
+	 GUI setup
+	 */
+	gui.setup();
+	gui.loadFont("Fonts/din-webfont.ttf", 11);
+	gui.loadCameraSettings("settings.camera.xml");
+	
+	
+	/*!
+	 Ghost setup
+	 */
+	ghost.setDistance(150.0);
+	ghost.setSpeed(27.0);
+	ghost.setHistorySize(10);
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+	/* Entities update */
+	oscReceiver.update();
+	
+	
+	// update ghost with passing ramActor
+	ghost.update( getActor(myActorName) );
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+	// get processed ramActor ref
+	ramActor& actor = ghost.getActor();
+
+	ramCameraBegin();
+	{
+		ramBasicActor( actor, NULL );
+	}
+	ramCameraEnd();
+}
+
+
+
+#pragma mark - ram methods
+//--------------------------------------------------------------
+void testApp::drawFloor()
+{
+	ramBasicFloor(gui.getValueI("Floor pattern"),
+				  gui.getValueF("Floor size"),
+				  gui.getValueF("Grid size"),
+				  ramColor::BLUE_LIGHT,
+				  ramColor::BLUE_LIGHT-20);
+}
+
+//--------------------------------------------------------------
+void testApp::drawActor(ramActor &actor)
+{
+	ramBasicActor( actor, NULL );
+}
+
+//--------------------------------------------------------------
+void testApp::drawRigid(ramRigidBody &rigid)
+{
+	
+}
+
+
+
+
+#pragma mark - oF Events
+//--------------------------------------------------------------
+void testApp::keyPressed(int key)
+{
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo)
+{
+	
+}
+
