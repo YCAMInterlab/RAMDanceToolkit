2c40ac1047c3ce1b8ee978caff7f91f407536969
Satoru Higa <satoruhiga@gmail.com>
2013-02-11 00:55:34 +0900

    Update ramCylinderPrimitive graphics

--- libs/ram/Environment/ramPrimitive.h
+++ libs/ram/Environment/ramPrimitive.h
@@ -38,7 +38,17 @@ protected:
 class ramSpherePrimitive : public ramPrimitive
 {
 public:
-	ramSpherePrimitive(const ofVec3f& pos, float size = 100);
+	ramSpherePrimitive(const ofVec3f& pos, float radius = 100);
+	
+protected:
+	void customDraw();
+};
+
+class ramCylinderPrimitive : public ramPrimitive
+{
+public:
+	ramCylinderPrimitive(const ofMatrix4x4& mat, float radius = 100, float height = 100);
+	ramCylinderPrimitive(const ofVec3f& pos, float radius = 100, float height = 100);
 	
 protected:
 	void customDraw();
