7e19b8f52d4ec5b4769fec2a217dab924d70546e
Satoru Higa <satoruhiga@gmail.com>
2013-02-13 00:14:59 +0900

    Update ramNodeLine

--- libs/ram/Graphics/ramNodeLine.h
+++ libs/ram/Graphics/ramNodeLine.h
@@ -10,9 +10,11 @@ class ramNodeLine
 public:
 	
 	ramNodeFinder from;
-	ramNodeFinder cn0;
-	ramNodeFinder cn1;
+	ramNodeFinder control0;
+	ramNodeFinder control1;
 	ramNodeFinder to;
+	
+	ramNodeLine() {}
 	
 	float getDistance()
 	{
@@ -28,63 +30,119 @@ public:
 		return p0.distance(p1);
 	}
 
-	ofPolyline getLine()
+	ramNodeLine& line()
 	{
-		ofPolyline result;
+		polyline.clear();
 		
 		ramNode from_n;
 		ramNode to_n;
 		
-		if (!from.get(from_n)) return result;
-		if (!to.get(to_n)) return result;
+		if (!from.get(from_n)) return *this;
+		if (!to.get(to_n)) return *this;
 		
 		ofVec3f p0 = from_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
 		ofVec3f p1 = to_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
 
 		if (p0.distance(p1) < 1000)
 		{
-			result.addVertex(p0);
-			result.addVertex(p1);
+			polyline.addVertex(p0);
+			polyline.addVertex(p1);
 		}
 		
-		return result;
+		return *this;
 	}
 	
-	ofPolyline getCurve()
+	ramNodeLine& curve(float strength = 100)
 	{
-		ofPolyline result;
+		polyline.clear();
 		
 		ramNode from_n;
-		ramNode cn0_n;
-		ramNode cn1_n;
+		ramNode control0_n;
+		ramNode control1_n;
 		ramNode to_n;
 		
-		if (!from.get(from_n)) return result;
-		if (!to.get(to_n)) return result;
-		if (!cn0.get(cn0_n)) return result;
-		if (!cn1.get(cn1_n)) return result;
+		if (!from.get(from_n)) return *this;
+		if (!to.get(to_n)) return *this;
+		if (!control0.get(control0_n)) return *this;
+		if (!control1.get(control1_n)) return *this;
 		
 		ofVec3f p0 = from_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
 		ofVec3f p1 = to_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
 		
 		if (p0.distance(p1) < 1000)
 		{
-			float gain = 100; // style["curve_gain"].as<float>();
+			ofVec3f cp0 = control0_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
+			ofVec3f cp1 = control1_n.getGlobalTransformMatrix().preMult(-ofVec3f(0, 0, 0));
 			
-			ofVec3f cp0 = cn0_n.getGlobalTransformMatrix().preMult(ofVec3f(0, 0, 0));
-			ofVec3f cp1 = cn1_n.getGlobalTransformMatrix().preMult(-ofVec3f(0, 0, 0));
-			
-			ofVec3f pp0 = (p0 - cp0).normalized() * gain;
-			ofVec3f pp1 = (p1 - cp1).normalized() * gain;
+			ofVec3f pp0 = (p0 - cp0).normalized() * strength;
+			ofVec3f pp1 = (p1 - cp1).normalized() * strength;
 			
 			pp0 += p0;
 			pp1 += p1;
 			
-			result.addVertex(p0);
-			result.bezierTo(pp0, pp1, p1, 64);
+			polyline.addVertex(p0);
+			polyline.bezierTo(pp0, pp1, p1, 64);
 		}
 		
-		return result;
+		return *this;
 	}
+	
+	ramNodeLine& resampling(float spacing = 5)
+	{
+		spacing = ofClamp(spacing, 0.1, 100);
+		polyline = polyline.getResampledBySpacing(spacing);
+		return *this;
+	}
+	
+	ramNodeLine& spiral(float radius = 10, float freq = 10)
+	{
+		ofPolyline pp = polyline;
+		
+		float phase = 0;
+		for (int i = 0; i < polyline.size(); i++)
+		{
+			// FIXME: I'm not sure about this impl...
+			ofVec3f &v0 = polyline[i];
+			ofVec3f &v1 = polyline[i + 1];
+			
+			ofVec3f d = (v1 - v0);
+			ofVec3f c = d.crossed(ofVec3f(0, 1, 0)).normalized();
+			
+			c *= radius;
+			c.rotate(phase, ofVec3f(d));
+			
+			pp[i] = v0 + c;
+			
+			phase += freq;
+		}
+		
+		polyline = pp;
+		return *this;
+	}
+	
+	void draw()
+	{
+		polyline.draw();
+	}
+	
+	void drawDotted(int spaceing = 5)
+	{
+		if (polyline.size() > 0)
+		{
+			glBegin(GL_LINES);
+			for (int i = 0; i < polyline.size() - 1; i += spaceing)
+			{
+				ofVec3f &v0 = polyline[i];
+				ofVec3f &v1 = polyline[i + 1];
+				glVertex3fv(v0.getPtr());
+				glVertex3fv(v1.getPtr());
+			}
+			glEnd();
+		}
+	}
+	
+private:
+	
+	ofPolyline polyline;
 };
 
