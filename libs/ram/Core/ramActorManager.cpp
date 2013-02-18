abfb77d3befb040563db4030e4b5e2f4f23a1a3f
Satoru Higa <satoruhiga@gmail.com>
2013-02-19 03:51:46 +0900

    Rollback

--- libs/ram/Core/ramActorManager.cpp
+++ libs/ram/Core/ramActorManager.cpp
@@ -175,11 +175,8 @@ void ramActorManager::update()
 	rootNode.update();
 }
 
-#include "ramCameraManager.h"
 void ramActorManager::draw()
 {
-    ofPushView();
-    ramCameraManager::instance().getActiveCamera().begin(ofRectangle(0, 0, 1920, 1200));
 	rootNode.draw();
 	
 	if (nodeSelector->identifer.isValid())
@@ -203,9 +200,6 @@ void ramActorManager::draw()
 			node.endTransform();
 		}
 	}
-    
-    ramCameraManager::instance().getActiveCamera().end();
-    ofPopView();
 }
 
 void ramActorManager::updateWithOscMessage(const ofxOscMessage &m)
