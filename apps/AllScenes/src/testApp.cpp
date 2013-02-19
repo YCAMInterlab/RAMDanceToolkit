0cb3c7ed3e70b87ada3804083e8ac21875c1a5ab
Kyle McDonald <kyle@kylemcdonald.net>
2013-02-19 16:44:52 +0900

    added FourPoints and fixed bug with events in radio group

--- apps/AllScenes/src/testApp.cpp
+++ apps/AllScenes/src/testApp.cpp
@@ -49,6 +49,9 @@ ColorGrid colorGrid;
 #include "ThreePoints.h"
 ThreePoints threePoints;
 
+#include "FourPoints.h"
+FourPoints fourPoints;
+
 #pragma mark - oF methods
 //--------------------------------------------------------------
 void testApp::setup()
@@ -80,6 +83,7 @@ void testApp::setup()
 	scenes.push_back( hastyChase.getPtr() );
 	scenes.push_back( colorGrid.getPtr() );
 	scenes.push_back( threePoints.getPtr() );
+	scenes.push_back( fourPoints.getPtr() );
 	sceneManager.setup(scenes);
 }
 
