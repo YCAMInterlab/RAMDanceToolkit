a4809d06994e52e9c318d4a61a6d6bb0c668ff8a
Satoru Higa <satoruhiga@gmail.com>
2013-02-20 14:27:42 +0900

    Cleanup ramControlPanel

--- libs/ram/UI/ramControlPanel.cpp
+++ libs/ram/UI/ramControlPanel.cpp
@@ -16,15 +16,18 @@ ramOfxUIControlPanel::ramOfxUIControlPanel()
 	kDim = 16;
 	kXInit = OFX_UI_GLOBAL_WIDGET_SPACING;
 	kLength = 320-kXInit;
-	mR = 0;
-	mG = 0;
-	mB = 0;
-	mUseBgSlider = true;
+	
 	mFloorPattern = ramFloor::FLOOR_NONE;
 	mFloorSize = 600.0;
 	mGridSize = 50.0;
-	mLabelCamPos = new ofxUILabel("x:0 y:0 z:0", OFX_UI_FONT_MEDIUM);
 	enableShadow = true;
+	
+	fullScreen = false;
+	pause = false;
+	
+	camera_preset = camera_preset_t = 0;
+	
+	backgroundColor.set(0, 0, 0, 1);
 	
 	scenes = NULL;
 }
@@ -40,78 +43,59 @@ void ramOfxUIControlPanel::setup()
 	/// First panel
 	// -------------------------------------
 	/// panel
-	mPanelGeneral = new ofxUICanvas(0, 0, kLength+kXInit*2.0, ofGetScreenHeight());
-	mPanelGeneral->addWidgetDown(new ofxUILabel("RamDanceToolkit", OFX_UI_FONT_LARGE));
+	addPanel("RamDanceToolkit");
 	
+	addSection("RamDanceToolkit");
+	addToggle("FullScrean", &fullScreen);
+	addToggle("Pause (or press Space Key)", &pause);
+	addToggle("Use Shadow", &enableShadow);
+
+	addSeparator();
 	
-	/// full screan
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(new ofxUIToggle(32, 32, false, "FullScrean"));
+	addColorSelector("Background", &backgroundColor);
 	
-	
-	/// full screan
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(new ofxUIToggle(32, 32, false, "Pause (or press Space Key)"));
-	
-	
-	/// full screan
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(new ofxUIToggle(16, 16, &enableShadow, "Use Shadow"));
-	
-	
-	/// background color
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(new ofxUIToggle(32, 32, true, "Use Background Slider"));
-	mPanelGeneral->addSlider("BG:R", 0, 255, &mR, 95, kDim);
-	mPanelGeneral->setWidgetPosition(OFX_UI_WIDGET_POSITION_RIGHT);
-	mPanelGeneral->addSlider("BG:G", 0, 255, &mG, 95, kDim);
-	mPanelGeneral->addSlider("BG:B", 0, 255, &mB, 95, kDim);
-	mPanelGeneral->setWidgetPosition(OFX_UI_WIDGET_POSITION_DOWN);
-	
-	
+	addSeparator();
+
 	/// floor pattern
 	vector<string> floors = ramFloor::getFloorNames();
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addRadio("Floor Patterns", floors, OFX_UI_ORIENTATION_VERTICAL, kDim, kDim);
-	mPanelGeneral->addSlider("Floor Size", 100, 1000, &mFloorSize, kLength/2-kXInit, kDim);
-	mPanelGeneral->setWidgetPosition(OFX_UI_WIDGET_POSITION_RIGHT);
-	mPanelGeneral->addSlider("Grid Size", 20, 200, &mGridSize, kLength/2-kXInit, kDim);
-	mPanelGeneral->setWidgetPosition(OFX_UI_WIDGET_POSITION_DOWN);
+	addRadioGroup("Floor Patterns", floors, &mFloorPattern);
 	
+	current_panel->addSlider("Floor Size", 100, 1000, &mFloorSize, kLength/2-kXInit, kDim);
+	current_panel->setWidgetPosition(OFX_UI_WIDGET_POSITION_RIGHT);
+	current_panel->addSlider("Grid Size", 20, 200, &mGridSize, kLength/2-kXInit, kDim);
+	current_panel->setWidgetPosition(OFX_UI_WIDGET_POSITION_DOWN);
+	
+	addSeparator();
 	
 	/// camera Names
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addRadio("Camera Preset", ramCameraManager::instance().getDefaultCameraNames(), OFX_UI_ORIENTATION_VERTICAL, kDim, kDim);
-	
-	
-	/// Camera Position
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(new ofxUILabel("Camera Position", OFX_UI_FONT_MEDIUM));
-	mPanelGeneral->addWidgetDown(mLabelCamPos);
-
+	addRadioGroup("Camera Preset", ramCameraManager::instance().getDefaultCameraNames(), &camera_preset_t);
 	
 	/// add panel to canvas
-	mTabbedCanvas.add(mPanelGeneral);
 	mTabbedCanvas.loadSettings("GUI/guiSettings.xml");
-	
 	
 	/// Events
 	ofAddListener(ofEvents().keyPressed, this, &ramOfxUIControlPanel::keyPressed);
-	ofAddListener(mPanelGeneral->newGUIEvent, this, &ramOfxUIControlPanel::guiEvent);
+	ofAddListener(current_panel->newGUIEvent, this, &ramOfxUIControlPanel::guiEvent);
 }
 
 void ramOfxUIControlPanel::update(ofEventArgs &e)
 {
-	if (mUseBgSlider) ofBackground( ofColor(mR, mG, mB) );
+	if (fullScreen != (ofGetWindowMode() == OF_FULLSCREEN))
+	{
+		ofSetFullscreen(fullScreen);
+	}
 	
-	const ofVec3f &camPos = ramCameraManager::instance().getActiveCamera().getPosition();
-	stringstream pos;
-	pos <<
-	" X:" << (int)camPos.x <<
-	" Y:" << (int)camPos.y <<
-	" Z:" << (int)camPos.z << endl;
+	ramActorManager::instance().setFreezed(pause);
 	
-	mLabelCamPos->setLabel( pos.str() );
+	if (camera_preset_t != camera_preset)
+	{
+		camera_preset = camera_preset_t;
+		reloadCameraSetting(camera_preset);
+	}
+	
+	ramEnableShadow(enableShadow);
+	
+	ofBackground(backgroundColor);
 }
 
 //
@@ -133,8 +117,7 @@ void ramOfxUIControlPanel::addPanel(const string& name)
 	ofxUICanvas *panel = new ofxUICanvas(0, 0, ramGetGUI().kLength+ramGetGUI().kXInit*2.0, ofGetScreenHeight());
 	current_panel = panel;
 	
-	panel->addWidgetDown(new ofxUILabel(name, OFX_UI_FONT_LARGE));
-	panel->addSpacer(kLength, 2);
+	addSection(name);
 	getTabbedCanvas().add(panel);
 }
 
@@ -164,12 +147,12 @@ void ramOfxUIControlPanel::addMultiToggle(const string& name, const vector<strin
 	assert(false);
 }
 
-struct Listener
+struct RadioGroupListener
 {
 	ofxUIRadio *o;
 	int *value;
 	
-	Listener(ofxUIRadio *o, int *value) : o(o), value(value) {}
+	RadioGroupListener(ofxUIRadio *o, int *value) : o(o), value(value) {}
 	
 	void handle(ofxUIEventArgs &e)
 	{
@@ -190,10 +173,9 @@ void ramOfxUIControlPanel::addRadioGroup(const string& name, const vector<string
 {
 	ofxUIRadio *o = current_panel->addRadio(name, content, OFX_UI_ORIENTATION_VERTICAL, kDim, kDim);
 	
-
 	// FIXME: memory leak
-	Listener *e = new Listener(o, value);
-	ofAddListener(current_panel->newGUIEvent, e, &Listener::handle);
+	RadioGroupListener *e = new RadioGroupListener(o, value);
+	ofAddListener(current_panel->newGUIEvent, e, &RadioGroupListener::handle);
 }
 
 void ramOfxUIControlPanel::addDropdown(const string& name, const vector<string>& content, int *value)
@@ -213,7 +195,7 @@ struct ColorSelectorListener
 	ofFloatColor color;
 	
 	ColorSelectorListener(ofxUIToggle* toggle, ofFloatColor *value)
-	: toggle(toggle), value(value) {}
+	: toggle(toggle), value(value), color(*value) {}
 	
 	void handle(ofxUIEventArgs &e)
 	{
@@ -232,7 +214,7 @@ struct ColorSelectorListener
 
 void ramOfxUIControlPanel::addColorSelector(const string& name, ofFloatColor *value)
 {
-	current_panel->addWidgetDown(new ofxUILabel(name, OFX_UI_FONT_SMALL));
+	current_panel->addWidgetDown(new ofxUILabel(name, OFX_UI_FONT_MEDIUM));
 	
 	ofxUIToggle* toggle = current_panel->addToggle("", true, 26, 26);
 	current_panel->setWidgetPosition(OFX_UI_WIDGET_POSITION_RIGHT);
@@ -241,9 +223,9 @@ void ramOfxUIControlPanel::addColorSelector(const string& name, ofFloatColor *va
 	ColorSelectorListener *e = new ColorSelectorListener(toggle, value);
 	ofAddListener(current_panel->newGUIEvent, e, &ColorSelectorListener::handle);
 	
-	current_panel->addSlider("R", 0, 1, &value->r, 95, kDim);
-	current_panel->addSlider("G", 0, 1, &value->g, 95, kDim);
-	current_panel->addSlider("B", 0, 1, &value->b, 95, kDim);
+	current_panel->addSlider("R", 0, 1, &value->r, 90, kDim);
+	current_panel->addSlider("G", 0, 1, &value->g, 90, kDim);
+	current_panel->addSlider("B", 0, 1, &value->b, 90, kDim);
 	current_panel->setWidgetPosition(OFX_UI_WIDGET_POSITION_DOWN);
 }
 
@@ -267,8 +249,8 @@ void ramOfxUIControlPanel::setupSceneToggles(vector<ramBaseScene*>& scenes_)
 	
 	if (size <= 0)
 	{
-		mPanelGeneral->addSpacer(kLength, 2);
-		mPanelGeneral->addLabel("No scenes are assigned.");
+		current_panel->addSpacer(kLength, 2);
+		current_panel->addLabel("No scenes are assigned.");
 		return;
 	}
 	
@@ -276,46 +258,13 @@ void ramOfxUIControlPanel::setupSceneToggles(vector<ramBaseScene*>& scenes_)
 	const int numRow = ceil((float)size / numCol);
 	
 	mSceneToggles = new ofxUIToggleMatrix(kDim*3, kDim*2, numRow, numCol, "Scenes");
-	mPanelGeneral->addSpacer(kLength, 2);
-	mPanelGeneral->addWidgetDown(mSceneToggles);
+	current_panel->addSpacer(kLength, 2);
+	current_panel->addWidgetDown(mSceneToggles);
 }
 
 void ramOfxUIControlPanel::guiEvent(ofxUIEventArgs &e)
 {
 	string name = e.widget->getName();
-	
-	if ( name == "Use Background Slider" )
-	{
-		ofxUIToggle *toggle = (ofxUIToggle *) e.widget;
-		mUseBgSlider = toggle->getValue();
-	}
-	
-	if ( name == "FullScrean" )
-	{
-		ofxUIToggle *toggle = (ofxUIToggle *) e.widget;
-		ofSetFullscreen(toggle->getValue());
-	}
-
-	
-	/// floor patterns
-	if		( name == "NONE" )			 mFloorPattern = ramFloor::FLOOR_NONE;
-	else if ( name == "PLANE" )			 mFloorPattern = ramFloor::FLOOR_PLANE;
-	else if ( name == "CHECKER_PATTERN" )mFloorPattern = ramFloor::FLOOR_CHECKER_PATTERN;
-	else if ( name == "GRID_LINES" )	 mFloorPattern = ramFloor::FLOOR_GRID_LINES;
-	
-	
-	/// camera settings
-	if		( name == "CAM_FRONT" )		reloadCameraSetting( 0 );
-	else if ( name == "CAM_RIGHT" )		reloadCameraSetting( 1 );
-	else if ( name == "CAM_BACK" )		reloadCameraSetting( 2 );
-	else if ( name == "CAM_LEFT" )		reloadCameraSetting( 3 );
-	else if ( name == "CAM_TOP" )		reloadCameraSetting( 4 );
-	else if ( name == "CAM_BOTTOM" )	reloadCameraSetting( 5 );
-	else if ( name == "CAM_EDGE_FR" )	reloadCameraSetting( 6 );
-	else if ( name == "CAM_EDGE_BR" )	reloadCameraSetting( 7 );
-	else if ( name == "CAM_EDGE_BL" )	reloadCameraSetting( 8 );
-	else if ( name == "CAM_EDGE_FL" )	reloadCameraSetting( 9 );
-	
 	
 	/// scene togglematrix
 	if (scenes != NULL)
@@ -331,19 +280,17 @@ void ramOfxUIControlPanel::guiEvent(ofxUIEventArgs &e)
 			scene->setEnabled( toggles.at(i)->getValue() );
 		}
 	}
-	
-	if (name == "Pause (or press Space Key)")
-	{
-		ramActorManager::instance().toggleFreeze();
-	}
-	
-	ramEnableShadow(enableShadow);
 }
 
 void ramOfxUIControlPanel::keyPressed(ofKeyEventArgs &e)
 {
 	if (e.key == ' ')
 	{
-		ramActorManager::instance().toggleFreeze();
+		pause = !pause;
+	}
+	
+	if (e.key == '\t')
+	{
+		mTabbedCanvas.toggleVisible();
 	}
 }
