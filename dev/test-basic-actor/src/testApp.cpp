fa8be2a2b504767338b109ee27bffd7f2db5fe77
Satoru Higa <satoruhiga@gmail.com>
2013-02-20 13:41:31 +0900

    Add test-basic-actor project

---
+++ dev/test-basic-actor/src/testApp.cpp
@@ -0,0 +1,188 @@
+#include "testApp.h"
+
+#pragma mark - oF methods
+//--------------------------------------------------------------
+void testApp::setup()
+{
+	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofBackground(ramColor::WHITE);
+	
+	
+	/// ram setup
+	// ------------------
+	ramInitialize();
+	
+	oscReceiver.setup(10000);
+
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+	/// Entities update
+	// ------------------
+	oscReceiver.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+}
+
+#pragma mark - ram methods
+
+//--------------------------------------------------------------
+void testApp::drawActor(ramActor &actor)
+{
+//	ramDrawBasicActor(actor);
+
+	ofPushStyle();
+	
+	ofNoFill();
+	ofSetRectMode(OF_RECTMODE_CENTER);
+	
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
+	glEnable(GL_CULL_FACE);
+	
+	ofColor front_color(ramColor::BLUE_LIGHT, 200);
+	ofColor back_color(ramColor::WHITE, 200);
+	
+	for (int i = 0; i < actor.getNumNode(); i++)
+	{
+		ofSetColor(front_color);
+		
+		ramNode &node = actor.getNode(i);
+		ramNode *parent = node.getParent();
+		if (parent == NULL) continue;
+		
+		ramBox(node, 2);
+		
+		parent->beginTransform();
+		
+		ofVec3f axis(0, 0, 1);
+		ofVec3f c = node.getPosition().normalized().crossed(axis);
+		
+		ofRotate(90, c.x, c.y, c.z);
+		
+		ofVec3f p0 = node.getGlobalPosition();
+		ofVec3f p1 = parent->getGlobalPosition();
+		
+		float dist = p0.distance(p1);
+		float offset = 0.2;
+		int num = 4;
+		
+		ofLine(ofVec3f(0), ofVec3f(0, 0, -dist));
+		
+		if (i < 4)
+			glScalef(1., 1.8, 1);
+		else if (i == 4)
+			glScalef(1, 1, 3);
+		else
+			glScalef(1., 0.8, 1);
+		
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int n = 0; n < 6; n++)
+		{
+			float d = ofMap(n, 0, 5, 0, 1);
+			float dd = ofMap(d, 0, 1, offset, 1 - offset);
+			
+			float xx = sin(d * PI) * 2 + 4;
+			float zz = dd * -dist;
+			float w = 5;
+			
+			glVertex3f(xx, w, zz);
+			glVertex3f(xx, -w, zz);
+		}
+		glEnd();
+		
+		ofSetColor(back_color);
+		
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int n = 0; n < 6; n++)
+		{
+			float d = ofMap(n, 0, 5, 0, 1);
+			float dd = ofMap(d, 0, 1, offset, 1 - offset);
+			
+			float xx = -sin(d * PI) * 1 - 6;
+			float zz = dd * -dist;
+			float w = 3;
+			
+			glVertex3f(xx, -w, zz);
+			glVertex3f(xx, w, zz);
+		}
+		glEnd();
+
+
+		parent->endTransform();
+	}
+	
+	glPopAttrib();
+	
+	ofPopStyle();
+}
+
+//--------------------------------------------------------------
+void testApp::drawRigid(ramRigidBody &rigid)
+{
+	
+}
+
+
+
+
+#pragma mark - oF Events
+//--------------------------------------------------------------
+void testApp::keyPressed(int key)
+{
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg)
+{
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo)
+{
+	
+}
+
