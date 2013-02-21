7c060e163538893751403949600df4cfab7dafa5
Satoru Higa <satoruhiga@gmail.com>
2013-02-22 01:37:00 +0900

    Fix particle scene flicking bug

--- libs/ram/Core/ramGlobalShortcut.h
+++ libs/ram/Core/ramGlobalShortcut.h
@@ -15,22 +15,15 @@ struct ramGlobalShortcut
 	
 	inline static ramActorManager& getActorManager() { return ramActorManager::instance(); }
 	
-	//	inline bool hasActor(const string &key) { return ramActorManager::instance().hasActor(key); }
-	//	inline bool hasRigidBody(const string &key) { return ramActorManager::instance().hasRigidBody(key); }
 	inline static bool hasNodeArray(const string &key) { return ramActorManager::instance().hasNodeArray(key); }
 	
-	//	inline ramActor& getActor(string name) { return ramActorManager::instance().getActor(name); }
-	//	inline ramRigidBody& getRigidBody(string name) { return ramActorManager::instance().getRigidBody(name); }
 	inline static ramNodeArray& getNodeArray(string name) { return ramActorManager::instance().getNodeArray(name); }
 	
-	//	inline size_t getNumActor() { return ramActorManager::instance().getNumActor(); }
-	//	inline size_t getNumRigidBody() { return ramActorManager::instance().getNumRigidBody(); }
 	inline static size_t getNumNodeArray() { return ramActorManager::instance().getNumNodeArray(); }
 	
-	//	inline ramActor& getActor(int index) { return ramActorManager::instance().getActor(index); }
-	//	inline ramRigidBody& getRigidBody(int index) { return ramActorManager::instance().getRigidBody(index); }
 	inline static ramNodeArray& getNodeArray(int index) { return ramActorManager::instance().getNodeArray(index); }
 
+	inline static vector<ramNodeArray> getAllNodeArrays() { return ramActorManager::instance().getAllNodeArrays(); }
 	
 #pragma mark -
 #pragma mark Shortcut to RamCameraManager
