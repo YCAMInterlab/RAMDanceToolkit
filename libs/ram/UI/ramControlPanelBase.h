f00edb46795f39d3d4b99cf4d485a77edd147fb2
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-02-28 12:55:54 +0900

    cleaned up testApp, the code from shooting branch was mixed

--- libs/ram/UI/ramControlPanelBase.h
+++ libs/ram/UI/ramControlPanelBase.h
@@ -14,24 +14,17 @@ public:
 
 	virtual void addPanel(const string& name) {}
 	virtual void addSection(const string& name) {}
-
 	virtual void addSeparator() {}
-
-	virtual void addLabel(const string& content) {}
-
+	
 	template <typename Functor>
 	void addButton(const string& name, const Functor &functor) {}
-
+	virtual void addLabel(const string& content) {}
 	virtual void addToggle(const string& name, bool *value) {}
-
 	virtual void addMultiToggle(const string& name, const vector<string>& content, int *value) {}
-
 	virtual void addRadioGroup(const string& name, const vector<string>& content, int *value) {}
 	virtual void addDropdown(const string& name, const vector<string>& content, int *value) {}
-
 	virtual void addSlider(const string& name, float min_value, float max_value, float *value) {}
 	virtual void addColorSelector(const string& name, ofFloatColor *value) {}
-
 	virtual void remove(const string& name) {}
 
 	// accessor
