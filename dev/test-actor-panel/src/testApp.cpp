b1d14ac777e3e3c5b6a1220ec7fa60c587090969
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-03-03 16:54:41 +0900

    added dev/test-actor-panel

---
+++ dev/test-actor-panel/src/testApp.cpp
@@ -0,0 +1,93 @@
+#include "testApp.h"
+
+
+#include "ActorsPanel.h"
+ActorsPanel AR;
+
+#pragma mark - oF methods
+//--------------------------------------------------------------
+void testApp::setup()
+{
+	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofBackground(ramColor::WHITE);
+	
+	/// ram setup
+	// ------------------
+	ramInitialize(10000);
+	
+	vector<ramBaseScene*>scenes;
+	scenes.push_back(AR.getPtr());
+	SM.setup(scenes);
+	
+	AR.enable();
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+	SM.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+	SM.draw();
+}
+
+#pragma mark - ram methods
+//--------------------------------------------------------------
+void testApp::drawActor(const ramActor &actor)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::drawRigid(const ramRigidBody &rigid)
+{
+}
+
+#pragma mark - oF Events
+//--------------------------------------------------------------
+void testApp::keyPressed(int key)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg)
+{
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo)
+{
+}
