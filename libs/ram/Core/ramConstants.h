0a16e0b081f91a32d6d30cdc88240021b1fed646
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-03-03 12:41:28 +0900

    changed default settings

--- libs/ram/Core/ramConstants.h
+++ libs/ram/Core/ramConstants.h
@@ -5,7 +5,7 @@
 const std::string RAM_OSC_ADDR_ACTOR = "/ram/skeleton";
 const std::string RAM_OSC_ADDR_RIGID_BODY = "/ram/rigid_body";
 
-const float RAM_OUTDATED_DURATION = 0.2;
+const float RAM_OUTDATED_DURATION = 1.0;
 
 // ocs mesasge address
 const string addrTagName = "address";
