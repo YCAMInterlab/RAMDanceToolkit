389af4a808978284372e24b164b6a1978d4a0245
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-03-08 16:01:43 +0900

    apply ofxUI.patch

--- submodules.sh
+++ submodules.sh
@@ -2,11 +2,21 @@
 
 # usage: $ ./submodules.sh
 
+
+# submodule init & update
 echo "[start submodule init]"
 git submodule init
 echo "end submodule init."
 
-
 echo "[start submodule update]"
 git submodule update
 echo "end submodule update."
+
+
+# apply ofxUI patch
+
+echo "[start running patch for modify ofxUI]"
+cd addons/ofxUI
+patch -p0 < ../ofxUI.patch
+cd ../../
+echo "end apply patch."
