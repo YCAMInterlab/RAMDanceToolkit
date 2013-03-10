932937109a8ac3c7602eeebf1fd8a903d1942d36
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-03-10 16:49:31 +0900

    recognizer example

--- examples/example-ramRecognizer/src/testApp.cpp
+++ examples/example-ramRecognizer/src/testApp.cpp
@@ -1,5 +1,14 @@
 #include "testApp.h"
 
+
+ramMovementAnalyser movement;
+ramTimerdMovementAnalyser timerdMovement;
+
+ramNode nodeMov;
+ramNode nodeTMov;
+float mov;
+float tMov;
+
 #pragma mark - oF methods
 //--------------------------------------------------------------
 void testApp::setup()
@@ -10,18 +19,48 @@ void testApp::setup()
 	/// ram setup
 	// ------------------
 	ramInitialize(10000);
+	
+	movement.setJointID(ramActor::JOINT_LEFT_ANKLE);
+	timerdMovement.setJointID(ramActor::JOINT_HEAD);
+	
+	ramGetGUI().addPanel( &movement );
+	ramGetGUI().addPanel( &timerdMovement );
 }
 
 //--------------------------------------------------------------
 void testApp::update()
 {
-	
+	if (getNumNodeArray() > 0)
+	{
+		const string name = getNodeArray(0).getName();
+		movement.setTargetName( name );
+		timerdMovement.setTargetName( name );
+	}
+
+	mov = movement.update();
+	tMov = timerdMovement.update();
 }	
 
 //--------------------------------------------------------------
 void testApp::draw()
 {
 	
+	ramBeginCamera();
+	
+	// debugDraw
+	ofSetColor(100);
+	movement.draw();
+	timerdMovement.draw();
+	
+	// see also ramNodeFinder
+	movement.findOne(nodeMov);
+	timerdMovement.findOne(nodeTMov);
+	
+	// draw value
+	ofSetColor(255);
+	ofDrawBitmapString("mov: "+ofToString(mov), nodeMov.getGlobalPosition()+30);
+	ofDrawBitmapString("tMov: "+ofToString(tMov), nodeTMov.getGlobalPosition()+30);
+	ramEndCamera();
 }
 
 
@@ -29,7 +68,8 @@ void testApp::draw()
 //--------------------------------------------------------------
 void testApp::drawActor(const ramActor &actor)
 {
-	
+	ofSetColor(100);
+	ramDrawBasicActor(actor);
 }
 
 //--------------------------------------------------------------
