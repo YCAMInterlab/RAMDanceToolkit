655ad2288a1f58d52d8324375ec132c923569f4c
Motoi Shimizu <shimizu.motoi@gmail.com>
2013-04-09 21:53:00 +0900

    added node cam

--- libs/ram/UI/ramPresetTab.cpp
+++ libs/ram/UI/ramPresetTab.cpp
@@ -4,7 +4,10 @@
 
 ramPresetTab::ramPresetTab()
 :ofxUITab("Presets", false)
-,cameraPreset(1)
+,preset_cam_index(1)
+,use_node_cam(false)
+,cam_position(false)
+,cam_look_at(false)
 {
 	// should probably be a list of named presets instead of a grid
 	matrix = addToggleMatrix("Presets", 4, 4);
@@ -15,29 +18,104 @@ ramPresetTab::ramPresetTab()
 	cameraPresetNames.push_back("Low");
 	cameraPresetNames.push_back("High");
 	cameraPresetNames.push_back("Overhead");
-	cameraPresetRadio = addRadio("Camera position", cameraPresetNames);
-	cameraPresetRadio->getToggles()[cameraPreset]->setValue(true);
+	preset_cam_radio = addRadio("Camera position", cameraPresetNames);
+	preset_cam_radio->getToggles()[preset_cam_index]->setValue(true);
+	addSpacer();
 	
+    // nodecam
+    addLabel("1. Click a node on actor", OFX_UI_FONT_SMALL);
+    addLabel("which you want to set as", OFX_UI_FONT_SMALL);
+    addLabel("camera pos or lookAt.", OFX_UI_FONT_SMALL);
+    
+    addLabel("2. Click a button below.", OFX_UI_FONT_SMALL);
+    addLabel("a joint with red circle", OFX_UI_FONT_SMALL);
+    addLabel("will be set as pos or lookAt.", OFX_UI_FONT_SMALL);
+    addButton("Set Camera Position", &cam_position, 15, 15);
+    addButton("Set Camera LookAt", &cam_look_at, 15, 15);
+    
+    addLabel("3. Enable NodeCam.", OFX_UI_FONT_SMALL);
+    addToggle("Enable Node Cam", &use_node_cam, 30, 30);
+    
 	autoSizeToFitWidgets();
 	
-	ofAddListener(ofEvents().update, this, &ramPresetTab::setupCamera);
+	ofAddListener(ofEvents().update, this, &ramPresetTab::update);
+	ofAddListener(ofEvents().draw, this, &ramPresetTab::draw);
 	ofAddListener(newGUIEvent, this, &ramPresetTab::guiEvent);
 }
 
-void ramPresetTab::setupCamera(ofEventArgs& e) {
-	if(ofGetFrameNum() < 10) {
-		cameraPresetRadio->getToggles()[cameraPreset]->setValue(true);
-		cameraPresetRadio->getToggles()[cameraPreset]->triggerSelf();
-	} else {
-		ofRemoveListener(ofEvents().update, this, &ramPresetTab::setupCamera);
+void ramPresetTab::update(ofEventArgs& e)
+{
+    // why 10 frames?
+	if(ofGetFrameNum() < 10)
+    {
+		preset_cam_radio->getToggles()[preset_cam_index]->setValue(true);
+		preset_cam_radio->getToggles()[preset_cam_index]->triggerSelf();
 	}
+    
+    
+    // nodecam
+    if (cam_position)
+    {
+        nf_pos = ramActorManager::instance().getLastSelectedNodeIdentifer();
+        nf_pos.findOne(node_pos);
+    }
+    if (cam_look_at)
+    {
+        nf_look_at = ramActorManager::instance().getLastSelectedNodeIdentifer();
+        nf_look_at.findOne(node_look_at);
+    }
+    
+    if (use_node_cam)
+    {
+        ofVec3f pos = node_pos.getGlobalPosition();
+        ofVec3f lookAt = node_look_at.getGlobalPosition();
+        
+        ramCameraManager::instance().getActiveCamera().setGlobalPosition(pos);
+        ramCameraManager::instance().getActiveCamera().lookAt(lookAt);
+    }
 }
 
-void ramPresetTab::guiEvent(ofxUIEventArgs &e) {
-	int choice = getChoice(e, cameraPresetRadio);
-	if(choice != -1) {
+void ramPresetTab::draw(ofEventArgs& e)
+{
+    if (use_node_cam) return;
+    
+    ramBeginCamera();
+    glEnable(GL_DEPTH_TEST);
+    if (nf_pos.found())
+    {
+        ofVec3f pos = node_pos.getGlobalPosition();
+        pos.y += 20;
+        
+        ofPushStyle();
+        ofSetColor(255, 0, 0);
+        ofLine(node_pos, pos);
+        ofDrawBitmapString("[Camera]", pos);
+        ofPopStyle();
+    }
+
+    if (nf_look_at.found())
+    {
+        ofVec3f pos = node_look_at.getGlobalPosition();
+        pos.y += 20;
+        
+        ofPushStyle();
+        ofSetColor(255, 0, 0);
+        ofLine(node_look_at, pos);
+        ofDrawBitmapString("[LookAt]", pos);
+        ofPopStyle();
+    }
+    glDisable(GL_DEPTH_TEST);
+    ramEndCamera();
+}
+
+
+void ramPresetTab::guiEvent(ofxUIEventArgs &e)
+{
+	int choice = getChoice(e, preset_cam_radio);
+	if(choice != -1)
+    {
 		int indices[] = {0, 1, 5};
-		int choice = getChoice(cameraPresetRadio);
+		int choice = getChoice(preset_cam_radio);
 		ramCameraManager::instance().rollbackDefaultCameraSetting(indices[choice]);
 	}
 }
--- libs/ram/UI/ramPresetTab.h
+++ libs/ram/UI/ramPresetTab.h
@@ -1,14 +1,38 @@
 #pragma once
 
 #include "ofxUITabbedCanvas.h"
+#include "ramNodeFinder.h"
 
-class ramPresetTab : public ofxUITab {
+class ramPresetTab : public ofxUITab
+{
+    
 protected:
-	ofxUIRadio* cameraPresetRadio;
-	int cameraPreset;
+    
+    // preset scenes
 	ofxUIToggleMatrix* matrix;
+    
+    
+    // preset camera positions
+    ofxUIRadio* preset_cam_radio;
+	int preset_cam_index;
+    
+    
+    // nodecam
+    bool use_node_cam;
+    bool cam_position;
+    bool cam_look_at;
+    
+    ramNodeFinder nf_pos;
+    ramNodeFinder nf_look_at;
+    
+    ramNode node_pos;
+    ramNode node_look_at;
+    
 public:
+    
 	ramPresetTab();
-	void setupCamera(ofEventArgs& e);
+	void update(ofEventArgs& e);
+	void draw(ofEventArgs& e);
 	void guiEvent(ofxUIEventArgs &e);
+    
 };
