aafbe85164784de09116bd4b8579dda20d48df7c
itotaka <taka@ycam.jp>
2014-12-20 15:20:03 +0900

    Added ito's Scene

---
+++ appsDivisualPlays/Dec2014_ITO/src/scenes/HakoniwaOscTest.h
@@ -0,0 +1,31 @@
+//
+//  HakoniwaOscTest.h
+//  RAMDanceToolkit
+//
+//  Created by Onishi Yoshito on 7/22/14.
+//
+//
+
+#pragma once
+
+#include "ramMain.h"
+
+class HakoniwaOscTest : public ramBaseScene {       //
+public:
+    
+	HakoniwaOscTest();
+    virtual ~HakoniwaOscTest();
+    
+    void update();
+	void draw();
+    
+    void setupControlPanel();
+    void onPanelChanged(ofxUIEventArgs& e);
+    
+	inline
+    string getName() const { return "HakoniwaOscTest"; }
+    
+private:
+    ofxOscSender mOscSender;
+    bool mLed;
+};
