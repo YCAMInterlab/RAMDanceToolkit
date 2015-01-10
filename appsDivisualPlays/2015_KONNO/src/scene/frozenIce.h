72871f1fd19afcdb616b56f7038e48ccdf77d102
konnokeina <konno@ycam.jp>
2015-01-10 11:16:03 +0900

    make forzenIce

--- appsDivisualPlays/2015_KONNO/src/scene/frozenIce.h
+++ appsDivisualPlays/2015_KONNO/src/scene/frozenIce.h
@@ -9,6 +9,45 @@
 #ifndef __example_ramMotionExtractor__frozenIce__
 #define __example_ramMotionExtractor__frozenIce__
 
-#include <stdio.h>
+#include "ramMain.h"
+#include "ramMotionExtractor.h"
+
+class frozenIce : public ramBaseScene{
+public:
+    
+    frozenIce();
+    virtual ~frozenIce(){};
+    
+    void setup();
+    void setupControlPanel();
+    
+    void update();
+    void draw();
+    
+    inline string getName() const {
+        return "frozenIce";
+    }
+    
+    //--Ice---
+    int hantei;
+    bool iceStart;
+    int count;
+    
+private:
+    
+    ramMotionExtractor	motionExtractor;
+    ofxOscSender sender;
+    
+    bool mDrawLines;
+    bool mDrawTriangle;
+    bool mDrawPreview;
+    bool mDrawDump;
+    
+    void example_drawLines();
+    void example_drawTriangles();
+    void example_drawDump();
+    
+};
+
 
 #endif /* defined(__example_ramMotionExtractor__frozenIce__) */
