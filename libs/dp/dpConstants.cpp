d8e8f38c261a85934e726b5ef6849b7775a08a30
kezzardrix <ryokanda62@gmail.com>
2015-01-12 18:11:48 +0900

    add dpConstatns.cpp for dpColor

---
+++ libs/dp/dpConstants.cpp
@@ -0,0 +1,15 @@
+//
+//  dpConstants.cpp
+//  RAMDanceToolkit
+//
+//  Created by kezzardrix2 on 2015/01/12.
+//  Copyright (c) 2015年 YCAMInterlab. All rights reserved.
+//
+
+#include "dpConstants.h"
+
+const ofColor dpColor::MAIN_COLOR       = ofColor(255, 50, 150);
+const ofColor dpColor::PALE_PINK_LIGHT  = ofColor(255, 220, 235);
+const ofColor dpColor::PALE_PINK_HEAVY  = ofColor(255, 150, 200);
+const ofColor dpColor::DARK_PINK_LIGHT  = ofColor(200, 50, 120);
+const ofColor dpColor::DARK_PINK_HEAVY  = ofColor(130, 50, 80);
