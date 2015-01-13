1b86e95d06aeee028f869795ccb9b0169097e9ef
loveandsheep <sheep@tele-scope.jp>
2015-01-13 15:26:34 +0900

    add parallel link hakoniwas

---
+++ appsDivisualPlays/Dec2014_SHEEP/src/scenes/HakoniwaPLink_Prism.h
@@ -0,0 +1,14 @@
+//
+//  HakoniwaPLink_Prism.h
+//  RAMDanceToolkit
+//
+//  Created by Ovis aries on 2015/01/12.
+//
+//
+
+#ifndef __RAMDanceToolkit__HakoniwaPLink_Prism__
+#define __RAMDanceToolkit__HakoniwaPLink_Prism__
+
+#include <stdio.h>
+
+#endif /* defined(__RAMDanceToolkit__HakoniwaPLink_Prism__) */
---
+++ appsDivisualPlays/Dec2014_SHEEP/src/scenes/hakoVisPLink_Prism.h
@@ -0,0 +1,14 @@
+//
+//  hakoVisPLink_Prism.h
+//  RAMDanceToolkit
+//
+//  Created by Ovis aries on 2015/01/13.
+//
+//
+
+#ifndef __RAMDanceToolkit__hakoVisPLink_Prism__
+#define __RAMDanceToolkit__hakoVisPLink_Prism__
+
+#include <stdio.h>
+
+#endif /* defined(__RAMDanceToolkit__hakoVisPLink_Prism__) */
