3c68493b87dd2c9fc55d69fbc0b5775ba3ec65ab
Yoshito Onishi <g6136t@gmail.com>
2015-01-13 22:05:11 +0900

    Added body base scene

---
+++ appsDivisualPlays/dpScore/src/scenes/dpScoreSceneBodyBase.cpp
@@ -0,0 +1,14 @@
+//
+//  dpScoreSceneBodyBase.cpp
+//  dpScore
+//
+//  Created by YoshitoONISHI on 1/13/15.
+//
+//
+
+#include "dpScoreSceneBodyBase.h"
+
+DP_SCORE_NAMESPACE_BEGIN
+
+
+DP_SCORE_NAMESPACE_END
