16e8b5335a52cd2f64c4733c745eea58c6290c71
Yoshito Onishi <g6136t@gmail.com>
2015-01-14 22:17:46 +0900

    More align functions

--- appsDivisualPlays/dpScore/src/core/dpScoreCommon.cpp
+++ appsDivisualPlays/dpScore/src/core/dpScoreCommon.cpp
@@ -133,17 +133,33 @@ string demangle(const char* name)
     return (status==0) ? result.p : name;
 }
 
-float alignf(float f)
+float aligned(float f)
 {
-    return ::ceil(::floor(f));
+    //return ::ceilf(::floorf(f));
+    return ::floorf(f);
 }
 
-void alignedRect(const ofRectangle & r)
+void alignedLine(const ofPoint& p1, const ofPoint& p2)
+{
+    alignedLine(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
+}
+
+void alignedLine(float x1, float y1, float x2, float y2)
+{
+    alignedLine(x1, y1, 0.0f, x2, y2, 0.0f);
+}
+
+void alignedLine(float x1, float y1, float z1, float x2, float y2, float z2)
+{
+    ofLine(aligned(x1), aligned(y1), aligned(z1), aligned(x2), aligned(y2), aligned(z2));
+}
+
+void alignedRect(const ofRectangle& r)
 {
     alignedRect(r.x,r.y,0.0f,r.width, r.height);
 }
 
-void alignedRect(const ofPoint & p,float w,float h)
+void alignedRect(const ofPoint& p,float w,float h)
 {
     alignedRect(p.x, p.y, p.z, w, h);
 }
@@ -156,7 +172,7 @@ void alignedRect(float x,float y,float w,float h)
 
 void alignedRect(float x,float y,float z,float w,float h)
 {
-    ofRect(alignf(x), alignf(y), alignf(z), alignf(w), alignf(h));
+    ofRect(aligned(x), aligned(y), aligned(z), aligned(w), aligned(h));
 }
 
 void alignedTranslate(const ofPoint& p)
@@ -166,7 +182,37 @@ void alignedTranslate(const ofPoint& p)
 
 void alignedTranslate(float x, float y, float z)
 {
-    ofTranslate(alignf(x), alignf(y), alignf(z));
+    ofTranslate(aligned(x), aligned(y), aligned(z));
+}
+
+ofVec2f alignedVec2f(const ofVec2f& p)
+{
+    return ofVec2f(aligned(p.x), aligned(p.y));
+}
+
+ofVec2f alignedVec2f(float x, float y)
+{
+    return ofVec2f(aligned(x), aligned(y));
+}
+
+ofVec3f alignedVec3f(const ofVec3f& p)
+{
+    return ofVec3f(aligned(p.x), aligned(p.y), aligned(p.z));
+}
+
+ofVec3f alignedVec3f(float x, float y, float z)
+{
+    return ofVec3f(x, y, z);
+}
+
+ofRectangle alignedRectangle(float px, float py, float w, float h)
+{
+    return ofRectangle(aligned(px), aligned(py), aligned(w), aligned(h));
+}
+
+ofRectangle alignedRectangle(const ofPoint& p, float w, float h)
+{
+    return ofRectangle(alignedVec3f(p), aligned(w), aligned(h));
 }
         
 ofVec3f randVec3f()
--- appsDivisualPlays/dpScore/src/core/dpScoreCommon.h
+++ appsDivisualPlays/dpScore/src/core/dpScoreCommon.h
@@ -51,15 +51,28 @@ string demangle(const char* name);
 template <class T> string getClassName(const T& t);
 template <class T> string getClassName();
 
-float alignf(float f);
+float aligned(float f);
 
-void alignedRect(const ofRectangle & r);
-void alignedRect(const ofPoint & p,float w,float h);
-void alignedRect(float x,float y,float w,float h);
-void alignedRect(float x,float y,float z,float w,float h);
+void alignedLine(const ofPoint& p1, const ofPoint& p2);
+void alignedLine(float x1, float y1, float x2, float y2);
+void alignedLine(float x1, float y1, float z1, float x2, float y2, float z2);
+
+void alignedRect(const ofRectangle& r);
+void alignedRect(const ofPoint& p, float w, float h);
+void alignedRect(float x, float y, float w, float h);
+void alignedRect(float x, float y, float z, float w, float h);
 
 void alignedTranslate(const ofPoint& p);
 void alignedTranslate(float x, float y, float z = 0.f);
+
+ofVec2f alignedVec2f(const ofVec2f& p);
+ofVec2f alignedVec2f(float x, float y);
+
+ofVec3f alignedVec3f(const ofVec3f& p);
+ofVec3f alignedVec3f(float x, float y, float z = 0.f);
+
+ofRectangle alignedRectangle(float px, float py, float w, float h);
+ofRectangle alignedRectangle(const ofPoint& p, float w, float h);
 
 ofVec3f randVec3f();
 
