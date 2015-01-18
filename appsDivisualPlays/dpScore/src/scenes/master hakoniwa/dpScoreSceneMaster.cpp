ef1af4e031083f94fa6d304463a3ca971c6e3e72
Yoshito Onishi <g6136t@gmail.com>
2015-01-18 17:51:23 +0900

    Updated color of water sketch

---
+++ appsDivisualPlays/dpScore/src/scenes/master hakoniwa/dpScoreSceneMaster.cpp
@@ -0,0 +1,9 @@
+//
+//  dpScoreSceneMasterBody.cpp
+//  dpScore
+//
+//  Created by YoshitoONISHI on 1/18/15.
+//
+//
+
+#include "dpScoreSceneMasterBody.h"
---
+++ appsDivisualPlays/dpScore/src/scenes/master hakoniwa/dpScoreSceneMaster.h
@@ -0,0 +1,14 @@
+//
+//  dpScoreSceneMasterBody.h
+//  dpScore
+//
+//  Created by YoshitoONISHI on 1/18/15.
+//
+//
+
+#ifndef __dpScore__dpScoreSceneMasterBody__
+#define __dpScore__dpScoreSceneMasterBody__
+
+#include <stdio.h>
+
+#endif /* defined(__dpScore__dpScoreSceneMasterBody__) */
