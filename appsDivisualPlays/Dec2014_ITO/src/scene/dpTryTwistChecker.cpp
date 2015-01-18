88ebeab9088842d75b90da3c76cd0ccbe81b202e
itotaka <taka@ycam.jp>
2015-01-18 20:19:25 +0900

    update

---
+++ appsDivisualPlays/Dec2014_ITO/src/scene/dpTryTwistChecker.cpp
@@ -0,0 +1,9 @@
+//
+//  dpTryTwistChecker.cpp
+//  example-ramMotionExtractor
+//
+//  Created by itotaka on 1/18/15.
+//
+//
+
+#include "dpTryTwistChecker.h"
---
+++ appsDivisualPlays/Dec2014_ITO/src/scene/dpTryTwistChecker.h
@@ -0,0 +1,14 @@
+//
+//  dpTryTwistChecker.h
+//  example-ramMotionExtractor
+//
+//  Created by itotaka on 1/18/15.
+//
+//
+
+#ifndef __example_ramMotionExtractor__dpTryTwistChecker__
+#define __example_ramMotionExtractor__dpTryTwistChecker__
+
+#include <stdio.h>
+
+#endif /* defined(__example_ramMotionExtractor__dpTryTwistChecker__) */
