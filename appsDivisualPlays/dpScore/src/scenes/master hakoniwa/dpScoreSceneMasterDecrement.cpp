4eb0093a2c187eff488a4123d57b1feb5441d2b2
Yoshito Onishi <g6136t@gmail.com>
2015-01-19 18:58:31 +0900

    Clean up UI

---
+++ appsDivisualPlays/dpScore/src/scenes/master hakoniwa/dpScoreSceneMasterDecrement.cpp
@@ -0,0 +1,9 @@
+//
+//  dpScoreSceneMasterDecrement.cpp
+//  dpScore
+//
+//  Created by YoshitoONISHI on 1/19/15.
+//
+//
+
+#include "dpScoreSceneMasterDecrement.h"
---
+++ appsDivisualPlays/dpScore/src/scenes/master hakoniwa/dpScoreSceneMasterDecrement.h
@@ -0,0 +1,13 @@
+//
+//  dpScoreSceneMasterDecrement.h
+//  dpScore
+//
+//  Created by YoshitoONISHI on 1/19/15.
+//
+//
+
+#ifndef dpScore_dpScoreSceneMasterDecrement_h
+#define dpScore_dpScoreSceneMasterDecrement_h
+
+
+#endif
