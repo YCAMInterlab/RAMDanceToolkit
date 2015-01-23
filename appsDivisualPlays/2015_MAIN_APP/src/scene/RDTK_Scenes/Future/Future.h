309a5314a086ec7e5d153296cd0a2a6f4ce86c17
kezzardrix <ryokanda62@gmail.com>
2015-01-23 17:24:26 +0900

    getRDTKSceneCameraLookAt(),getRDTKSceneCameraPosition() in dpConstants.h

--- appsDivisualPlays/2015_MAIN_APP/src/scene/RDTK_Scenes/Future/Future.h
+++ appsDivisualPlays/2015_MAIN_APP/src/scene/RDTK_Scenes/Future/Future.h
@@ -148,8 +148,8 @@ public:
 	}
 	
     void onEnabled(){
-        ramCameraManager::instance().getActiveCamera().setPosition(0,240,420);
-        ramCameraManager::instance().getActiveCamera().lookAt(ofPoint(0,100,0));
+        ramCameraManager::instance().getActiveCamera().setPosition(getRDTKSceneCameraPosition());
+        ramCameraManager::instance().getActiveCamera().lookAt(getRDTKSceneCameraLookAt());
     }
 	
 	string getName() const { return "Future"; }
