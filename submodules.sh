cf829a67d51adacac30de77cd2276541d2ce5ee5
Yoshito Onishi <g6136t@gmail.com>
2015-02-04 14:24:17 +0900

    Updated submodules.sh

--- submodules.sh
+++ submodules.sh
@@ -22,42 +22,42 @@ else
 	echo "[couldn't found ./.gitmodule. start clone five addons from github repos instead of git submodule update]"
 
 	# ofxBt
-	git clone git@github.com:motoishmz/ofxBt.git
+	git clone https://github.com/motoishmz/ofxBt.git
 	cd ofxBt
 	cd ../
 
 	# ofxUI
-	git clone git@github.com:rezaali/ofxUI.git
+	git clone https://github.com/rezaali/ofxUI.git
 	cd ofxUI
 	git checkout 1832f2a
 	cd ../
 
 	# ofxInteractivePrimitives
-	git clone git@github.com:satoruhiga/ofxInteractivePrimitives.git
+	git clone https://github.com/satoruhiga/ofxInteractivePrimitives.git
 	cd ofxInteractivePrimitives
 	git checkout 987205e
 	cd ../
 
 	# ofxCv
-	git clone git@github.com:kylemcdonald/ofxCv.git
+	git clone https://github.com/kylemcdonald/ofxCv.git
 	cd ofxCv
 	git checkout 115deaf
 	cd ../
 
 	# ofxNodeArray
-	git clone git@github.com:YCAMInterlab/ofxNodeArray.git
+	git clone https://github.com/YCAMInterlab/ofxNodeArray.git
 	cd ofxNodeArray
 	git checkout b21dcd5
 	cd ../
 
 	# ofxQuadWarp
-	git clone git@github.com:julapy/ofxQuadWarp.git
+	git clone http://github.com/julapy/ofxQuadWarp.git
 	cd ofxQuadWarp
 	git checkout a45ff5e
 	cd ../
 
 	# ofxDelaunay
-	git clone git@github.com:obviousjim/ofxDelaunay.git
+	git clone https://github.com/obviousjim/ofxDelaunay.git
 	cd ofxDelaunay
 	git checkout 957f902
 	cd ../
