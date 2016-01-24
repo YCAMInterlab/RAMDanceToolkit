3cb7a2e94d0cd1a894eac2e62e9ee88db80287a2
kezzardrix <ryokanda62@gmail.com>
2016-01-24 14:48:57 +0900

    dpVideoSyphonServer first commit. only single video source.

---
+++ DividualPlays/dpVideoSyphonServer/src/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  dpVideoSyphonServer
+//
+//  Created by kezzardrix2 on 2016/01/24.
+//
+//
+
+#ifndef dpVideoSyphonServer_Constants_h
+#define dpVideoSyphonServer_Constants_h
+
+static const int EXTERNAL_VIDEO_WIDTH = 640;
+static const int EXTERNAL_VIDEO_HEIGHT = 480;
+
+#endif
