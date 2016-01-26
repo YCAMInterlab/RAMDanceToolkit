7f83690f607b2bf43a5f6deb5003e9d1d79f8a7f
ycam <ycam@ycam.xx.xx>
2016-01-26 15:05:32 +0900

    fix tornado sender algorythm

--- DividualPlays/2015_MAIN_APP/src/scene/knd/dpHakoniwaTornado.h
+++ DividualPlays/2015_MAIN_APP/src/scene/knd/dpHakoniwaTornado.h
@@ -57,6 +57,7 @@ public:
         ofxOscMessage m;
         m.setAddress("/dp/hakoniwa/tornado/fan");
         m.addIntArg(val);
+        m.addIntArg(isBulb);
         mFanSender.sendMessage(m);
         
     }
@@ -122,8 +123,10 @@ public:
         
         if(mRad > mMistThresh){
             isMist = true;
+            mFan = 255;
         }else{
             isMist = false;
+            mFan = 0;
         }
         
         ramBeginCamera();
@@ -131,7 +134,7 @@ public:
         ofTranslate(center);
         rotateToNormal(normal);
         ofNoFill();
-        ofCircle(0,0,radius,radius);
+      //  ofCircle(0,0,radius,radius);
         ofPopMatrix();
         
         ofSetColor(255,255,255);
