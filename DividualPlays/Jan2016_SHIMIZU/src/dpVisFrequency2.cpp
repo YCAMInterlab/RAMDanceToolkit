9027928a6d158f10aee4a264fa9d47151832affe
Junichi Shimizu <junichishimizu@Junichis-MacBook-Air.local>
2016-01-26 11:24:31 +0900

    memetetst

---
+++ DividualPlays/Jan2016_SHIMIZU/src/dpVisFrequency2.cpp
@@ -0,0 +1,9 @@
+//
+//  dpVisFrequency2.cpp
+//  RAMDanceToolkit
+//
+//  Created by Junichi Shimizu on 1/26/16.
+//
+//
+
+#include "dpVisFrequency2.h"
---
+++ DividualPlays/Jan2016_SHIMIZU/src/dpVisFrequency2.h
@@ -0,0 +1,14 @@
+//
+//  dpVisFrequency2.h
+//  RAMDanceToolkit
+//
+//  Created by Junichi Shimizu on 1/26/16.
+//
+//
+
+#ifndef __RAMDanceToolkit__dpVisFrequency2__
+#define __RAMDanceToolkit__dpVisFrequency2__
+
+#include <stdio.h>
+
+#endif /* defined(__RAMDanceToolkit__dpVisFrequency2__) */
