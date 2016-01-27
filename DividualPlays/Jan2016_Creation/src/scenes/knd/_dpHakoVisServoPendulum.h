4185f1046a1c96419e354d7529a2f2ad0ef98b65
kezzardrix <ryokanda62@gmail.com>
2016-01-27 20:58:10 +0900

    swap dpHakoVisServoPendulum to Old Scene

--- DividualPlays/Jan2016_Creation/src/scenes/knd/_dpHakoVisServoPendulum.h
+++ DividualPlays/Jan2016_Creation/src/scenes/knd/_dpHakoVisServoPendulum.h
@@ -9,61 +9,10 @@
 #ifndef RAMDanceToolkit_dpHakoVisRecordRibbon_h
 #define RAMDanceToolkit_dpHakoVisRecordRibbon_h
 
-class dpSPBox{
-public:
-    
-    dpSPBox(){
-        mColor = dpColor::MAIN_COLOR;
-    }
-    
-    void start(ofPoint size,float angle){
-        mSize = size;
-        mAngle = angle;
-        mSat.imSet(255);
-        mSat.set(0);
-        mSat.speed = 0.03;
-    }
-    
-    void update(){
-        mPos.z += mSpeed;
-        mSat.update();
-        
-        if(mSat.val > 1){
-            mColor.setSaturation(mSat.val);
-            mColor.setBrightness(ofMap(mSat.val,255,0,255,90));
-        }
-    }
-    
-    void draw(){
-    
-        ofSetColor(mColor.r,mColor.g,mColor.b,ofMap(mPos.z,230,300,255,0,true));
-        
-        ofPushMatrix();
-        ofTranslate(0, 0, mPos.z);
-        ofRotateZ(ofRadToDeg(mAngle));
-        ofSetRectMode(OF_RECTMODE_CENTER);
-        ofDrawBox(0,0,0,mSize.x,mSize.y,mSize.z);
-        ofPopMatrix();
-    
-        ofPushMatrix();
-        ofTranslate(0, 0, -mPos.z);
-        ofRotateZ(ofRadToDeg(mAngle));
-        ofSetRectMode(OF_RECTMODE_CENTER);
-        ofDrawBox(0,0,0,mSize.x,mSize.y,mSize.z);
-        ofPopMatrix();
-       
-    }
-    
-private:
-    ofPoint mPos;
-    ofPoint mSize;
-    
-    float mSpeed = 1.0;
-    float mAngle = 0.0;
-
-    ofColor mColor;
-    KezSlide mSat;
-};
+#include "KezLuminanceFilter.h"
+#include "KezAccumlateWeightedFilter.h"
+#include "ofxBlur.h"
+#include "dpSyphonClientManager.h"
 
 class dpHakoVisServoPendulum : public ramBaseScene{
 public:
@@ -71,29 +20,56 @@ public:
     
 	void setupControlPanel(){
         
-        mScale = 600;
+        ramGetGUI().addSlider("speed",1.0,20.0,&mSpeed);
+        ramGetGUI().addSlider("lumi_thresh",0.0,1.0,&mLumiThresh);
+        ramGetGUI().addToggle("drawRawCam", &mIsDrawRawCam);
+        ramGetGUI().addSlider("offsetX",0.0,SINGLE_SCREEN_WIDTH,&mOffsetX);
         
-        ramGetGUI().addSlider("scale", 100.0, 2000.0, &mScale);
-        ramGetGUI().addButton("rndOrbit");
-
-        mLong.speed = 0.005;
-        mLat.speed = 0.005;
-        mRad.speed = 0.01;
-        
-        rndOrbit();
+       // ramGetGUI().addSlider("dst",0.0,1.0,&mDst);
+       // ramGetGUI().addSlider("src",0.0,1.0,&mSrc);
         
         ofAddListener(ramGetGUI().getCurrentUIContext()->newGUIEvent, this, &dpHakoVisServoPendulum::onPanelChanged);
     }
     void setup(){
     
-     
-        mCam.setVFlip(true);
-        
         ramOscManager::instance().addReceiverTag(&mReceiver);
         mReceiver.addAddress("/dp/cameraUnit/ServoPendulum/vector/total");
     
-        mHead.speed = 0.2;
+        mAccumlateWeightedFilter.setup();
+        
+        mFbo.allocate(SINGLE_SCREEN_WIDTH,SINGLE_SCREEN_HEIGHT,GL_RGB32F);
+        mBufferFbo.allocate(SINGLE_SCREEN_WIDTH,SINGLE_SCREEN_HEIGHT,GL_RGB32F);
+        
+        mFbo.begin();
+        ofClear(0,0);
+        mFbo.end();
+        
+        mBufferFbo.begin();
+        ofClear(0,0);
+        mBufferFbo.end();
+        
+        mBlur.setup(SINGLE_SCREEN_WIDTH,SINGLE_SCREEN_HEIGHT,10,0.2,4);
+        mBlur.setBrightness(2.0);
+        mBlur.setup(SINGLE_SCREEN_WIDTH, SINGLE_SCREEN_HEIGHT);
+        mLumi.setup();
 
+        mFinalFbo.allocate(SINGLE_SCREEN_WIDTH, SINGLE_SCREEN_HEIGHT,GL_RGB);
+        
+        string frag = STRINGIFY(
+                                uniform sampler2DRect src;
+
+                                void main(void){
+                                    
+                                    vec2 st = gl_TexCoord[0].st;
+                                    vec4 color = texture2DRect(src,st);
+                                    if(dot(color.rgb,vec3(0.333)) < 0.01)discard;
+                                    
+                                    gl_FragColor = color;
+                                
+                                }
+                                );
+        mDiscards.setupShaderFromSource(GL_FRAGMENT_SHADER, frag);
+        mDiscards.linkProgram();
     }
     
     void receiveOsc(){
@@ -101,22 +77,6 @@ public:
         while(mReceiver.hasWaitingMessages()){
             ofxOscMessage m;
             mReceiver.getNextMessage(&m);
-            
-            if(m.getAddress() == "/dp/cameraUnit/ServoPendulum/vector/total"){
-                mBoxes.push_back(dpSPBox());
-                
-                mHead.set(m.getArgAsFloat(0),
-                          m.getArgAsFloat(1));
-                
-                float angle = atan2(mHead.y,mHead.x);
-                
-                mAngle = angle;
-                
-                mBoxes.back().start(ofPoint(1.0,mHead.length() * mScale,1.0),angle);
-                if(mBoxes.size() > BOX_MAX)mBoxes.pop_front();
-                
-                mHead.update();
-            }
         
         }
     }
@@ -124,85 +84,99 @@ public:
     void update(){
         
         receiveOsc();
+        
+        mAccumlateWeightedFilter.setSpeed(mSpeed);
+        mLumi.setThresh(mLumiThresh);
    
-        for(auto &v:mBoxes){
-            v.update();
-        }
-        
-        mLong.update();
-        mLat.update();
-        mRad.update();
-        
-        mCam.lookAt(ofPoint(0,0,0));
-        mCam.orbit(mLong.val, mLat.val, mRad.val);
-        
-        if(ofGetFrameNum() % RND_ROT_FRAME == 0)rndOrbit();
     }
     
-    void rndOrbit(){
-        mLong.set(ofRandom(-180,180));
-        mLat.set(ofRandom(-180,180));
-        mRad.set(ofRandom(50,400));
-    }
+  
     
     void draw(){
 
         ofDisableDepthTest();
-        ofEnableBlendMode(OF_BLENDMODE_ADD);
-        ofSetLineWidth(2);
-   
-        mCam.begin(dpGetFirstScreenViewPort());
+        mFbo.begin();
+        ofClear(0);
+        ofSetColor(255);
+        dpSyphonClientManager::instance().drawWithSideCrop(mOffsetX, 0);
+        mFbo.end();
         
-        ofSetColor(dpColor::MAIN_COLOR.r,
-                   dpColor::MAIN_COLOR.g,
-                   dpColor::MAIN_COLOR.b,
-                   255);
+        mAccumlateWeightedFilter.begin();
         
+        mLumi.begin(mBufferFbo.getTextureReference());
+        mFbo.draw(0,0,SINGLE_SCREEN_WIDTH,SINGLE_SCREEN_HEIGHT);
+        mLumi.end();
+        
+        mAccumlateWeightedFilter.end();
+        mAccumlateWeightedFilter.process();
+        
+        mBlur.begin();
+        ofClear(0);
         ofPushMatrix();
-        ofTranslate(0, 0, 0.0);
-        ofRotateZ(ofRadToDeg(mAngle));
-        ofDrawBox(0,0,0,1.0,mHead.length() * mScale,1.0);
+        ofScale(1.0,1.0);
+        mAccumlateWeightedFilter.draw();
         ofPopMatrix();
+        mBlur.end();
         
-        ofNoFill();
         
-        for(auto &v:mBoxes){
-            v.draw();
-        }
+        mFinalFbo.begin();
+        ofClear(0,0);
+        ofEnableBlendMode(OF_BLENDMODE_ADD);
+        mAccumlateWeightedFilter.draw();
+        mBlur.draw();
+        if(mIsDrawRawCam)dpSyphonClientManager::instance().drawWithSideCrop(mOffsetX, 0);
+        mFinalFbo.end();
         
-        mCam.end();
+        mDiscards.begin();
+        mFinalFbo.draw(0,0);
+        mDiscards.end();
         
+        drawToBuffer();
+ 
+    }
+    
+    
+    
+    void onEnabled(){
+        drawToBuffer();
+        mAccumlateWeightedFilter.clear();
     }
     
     void onPanelChanged(ofxUIEventArgs& e){
         
         string name = e.widget->getName();
-        
-        if(name == "rndOrbit")rndOrbit();
-        
+    
+
     }
     
 private:
-    
-    ofEasyCam mCam;
 
+    ofFbo mFbo;
+    ofFbo mBufferFbo;
+    
+    KezLuminanceFilter mLumi;
+    KezAccumlateWeightedFilter mAccumlateWeightedFilter;
+    
     ramOscReceiveTag mReceiver;
     
-    float mScale;
+    ofxBlur mBlur;
     
-    static const int BOX_MAX = 300;
-    deque<ofPoint>mPts;
-    deque<dpSPBox>mBoxes;
- 
-    KezSlidePoint mHead;
+    float mSpeed = 3.0;
+    float mLumiThresh = 0.1;
+    bool mIsDrawRawCam = false;
+    float mOffsetX = 570.0;
     
-    float mAngle = 0.0;
+    void drawToBuffer(){
+        ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+        mBufferFbo.begin();
+        ofClear(0);
+        ofSetColor(255);
+        dpSyphonClientManager::instance().drawWithSideCrop(mOffsetX, 0);
+        mBufferFbo.end();
+    }
     
-    KezSlide mLong;
-    KezSlide mLat;
-    KezSlide mRad;
-    
-    static const int RND_ROT_FRAME = 1200;
+    ofFbo mFinalFbo;
+    ofShader mDiscards;
 };
 
 #endif
