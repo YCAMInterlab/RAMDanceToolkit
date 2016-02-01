e429bd21ab3338ed1a55e3ce08e25be40ca78f0a
Ovis aries <culminate0523@gmail.com>
2016-02-01 19:24:30 +0900

    actor view method

--- DividualPlays/dpConductor/src/Presets.cpp
+++ DividualPlays/dpConductor/src/Presets.cpp
@@ -23,6 +23,14 @@ void dpConductor::setCamActorScene(ofPtr<sectionSet> s)
 	s->addScene(SCENE_ACTOR, false, false, false, false);
 }
 
+void dpConductor::setActorViewing(ofPtr<sectionSet> s, bool ando, bool kojiri, bool shimaji, bool miyashita)
+{
+	s->addTuneT(SCENE_ACTOR, "V_"+ACTOR_ANDO, ando);
+	s->addTuneT(SCENE_ACTOR, "V_"+ACTOR_KOJIRI, kojiri);
+	s->addTuneT(SCENE_ACTOR, "V_"+ACTOR_SHIMAJI, shimaji);
+	s->addTuneT(SCENE_ACTOR, "V_"+ACTOR_MIYASHITA, miyashita);
+}
+
 void dpConductor::setSections()
 {
 	//AddScene preset (Disp 1, Disp6, Disp2-3, Disp 4-5)
@@ -39,12 +47,14 @@ void dpConductor::setSections()
 	setCamActorScene(ns);
 	ns->addScene(SCENE_BIGBOX, true, true, false, true);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_KOJIRI, JOINT_RIGHT_ELBOW);
-
+	setActorViewing(ns, false, true, false, false);
+	
 #pragma mark 0030
 	/*=============================================================*/
 	ns = setBasicSection("fp-0030-yasu-in-disp-off", true, true);
 	setCamActorScene(ns);
-
+	setActorViewing(ns, false, true, false, false);
+	
 #pragma mark 0040
 	/*=============================================================*/
 	ns = setBasicSection("fp-0040-yoko-out", true, true);
@@ -52,6 +62,7 @@ void dpConductor::setSections()
 	ns->addScene(SCENE_BIGBOX, true, true, true, true);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_KOJIRI, JOINT_RIGHT_ELBOW);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_SHIMAJI, JOINT_RIGHT_ELBOW);
+	setActorViewing(ns, false, true, true, false);
 	
 #pragma mark 0050 TODO: ボックスサイズプリセット
 	/*=============================================================*/
@@ -61,11 +72,13 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
+	setActorViewing(ns, false, true, true, true);
 	
 #pragma mark 0070
 	/*=============================================================*/
 	ns = setBasicSection("fp-0070-dispClear", true, true);
 	setCamActorScene(ns);
+	setActorViewing(ns, false, false, false, false);
 	
 #pragma mark 0080
 	/*=============================================================*/
@@ -79,6 +92,7 @@ void dpConductor::setSections()
 	ns->addTuneF(SCENE_LINE, "Curve0", 0.0);
 	ns->addTuneF(SCENE_LINE, "ext_to0", 800.0);
 	ns->addTuneF(SCENE_LINE, "ext_from0", 800.0);
+	setActorViewing(ns, false, true, false, false);
 	
 #pragma mark 0090 TODO: 4P 長さ
 	ns = setBasicSection("fp-0090-line-4p", true, true);
@@ -108,12 +122,17 @@ void dpConductor::setSections()
 	ns->addTuneF(SCENE_LINE, "Curve1", 300.0);
 	ns->addTuneF(SCENE_LINE, "ext_to1", 800.0);
 	ns->addTuneF(SCENE_LINE, "ext_from1", 800.0);
+	ns->addTuneF(SCENE_FOURPT, "Twist resolution", 52.0);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension width", 3.47);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension height", 1.40);
+	setActorViewing(ns, true, true, true, true);
 
 #pragma mark 0100
 	ns = setBasicSection("fp-0010-off_proj", true, false);
 	setCamActorScene(ns);
 	ns->addScene(SCENE_LINE, false, false, true, false);
 	ns->addScene(SCENE_FOURPT, false, true, false, true);
+	setActorViewing(ns, true, true, true, true);
 	
 #pragma mark 0110
 	ns = setBasicSection("fp-0110-4pt-upside-bigbox", true, true);
@@ -139,6 +158,11 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
 	ns->addExtractor(SCENE_UPSIDE, ACTOR_KOJIRI, JOINT_NECK);
 	
+	ns->addTuneF(SCENE_FOURPT, "Twist resolution", 52.0);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension width", 3.47);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension height", 1.40);
+	setActorViewing(ns, true, true, true, true);
+	
 #pragma mark 0120
 	ns = setBasicSection("fp-0120-3pt-bigbox", true, true);
 	ns->addScene(SCENE_CAMERA, false, false, false, false);
@@ -159,7 +183,7 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
-	
+	setActorViewing(ns, false, true, true, true);
 
 #pragma mark - 0130 TODO:[DONE]ここ宮下さんだけモンスターインスタンス2つ作る
 	ns = setBasicSection("fp-0130-all-monster", true, true);
@@ -171,7 +195,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_MONSTER, ACTOR_SHIMAJI, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER, ACTOR_MIYASHITA, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER_2, ACTOR_MIYASHITA, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0140
 	ns = setBasicSection("fp-0140-monster-burst", true, true);
 	setCamActorScene(ns);
@@ -181,7 +206,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_MONSTER, ACTOR_KOJIRI, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER, ACTOR_MIYASHITA, JOINT_NECK);
 	ns->addExtractor(SCENE_BURST, ACTOR_SHIMAJI, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0150 TODO: ここ追跡スピード変わる？
 	ns = setBasicSection("fp-0150-chaser", true, true);
 	ns->addScene(SCENE_CAMERA, false, false, false, false);
@@ -191,7 +217,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_CHASER, ACTOR_SHIMAJI, JOINT_NECK);
 	ns->addExtractor(SCENE_CHASER, ACTOR_MIYASHITA, JOINT_NECK);
 	ns->addExtractor(SCENE_CHASER, ACTOR_ANDO, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0160 TODO: 個別設定できない？ & TOE付いてない
 	ns = setBasicSection("fp-0160-donuts", true, true);
 	setCamActorScene(ns);
@@ -203,7 +230,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_DONUTS, ACTOR_SHIMAJI, JOINT_ABDOMEN);
 	ns->addExtractor(SCENE_DONUTS, ACTOR_MIYASHITA, JOINT_LEFT_HAND);
 	ns->addExtractor(SCENE_DONUTS, ACTOR_MIYASHITA, JOINT_RIGHT_HAND);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0180
 	ns = setBasicSection("fp-0180-donuts_all_off", true, true);
 	setCamActorScene(ns);
@@ -212,12 +240,21 @@ void dpConductor::setSections()
 	ns = setBasicSection("fp-0190-bigbox", true, true);
 	setCamActorScene(ns);
 	ns->addScene(SCENE_BIGBOX, false, false, false, true);
-	
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_KOJIRI, JOINT_RIGHT_ELBOW);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_SHIMAJI, JOINT_RIGHT_ELBOW);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_NECK);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_HIP);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
+	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
+	setActorViewing(ns, false, true, true, true);
+
 #pragma mark 0200
 	ns = setBasicSection("fp-0200-kojiri-bigbox", true, true);
 	setCamActorScene(ns);
 	ns->addScene(SCENE_BIGBOX, false, true, false, true);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_KOJIRI, JOINT_RIGHT_ELBOW);
+	setActorViewing(ns, false, true, false, false);
 	
 #pragma mark 0210
 	ns = setBasicSection("fp-0210-yasu-in", true, true);
@@ -229,6 +266,7 @@ void dpConductor::setSections()
 	ns->addScene(SCENE_BIGBOX, false, true, false, true);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_KOJIRI, JOINT_RIGHT_ELBOW);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_SHIMAJI, JOINT_RIGHT_ELBOW);
+	setActorViewing(ns, false, true, true, false);
 	
 #pragma mark 0230
 	ns = setBasicSection("fp-0230-miyashita-in-bigbox", true, true);
@@ -241,10 +279,12 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
-	
+	setActorViewing(ns, false, true, true, true);
+
 #pragma mark 0240
 	ns = setBasicSection("fp-0240-disp-alloff", true, true);
 	setCamActorScene(ns);
+	setActorViewing(ns, false, true, true, true);
 
 #pragma mark 0250
 	ns = setBasicSection("fp-0250-kojiri-line", true, true);
@@ -257,8 +297,9 @@ void dpConductor::setSections()
 	ns->addTuneF(SCENE_LINE, "Curve0", 0.0);
 	ns->addTuneF(SCENE_LINE, "ext_to0", 800.0);
 	ns->addTuneF(SCENE_LINE, "ext_from0", 800.0);
+	setActorViewing(ns, false, true, false, false);
 	
-#pragma mark 0260 TODO: 宮下さんLineパラメ
+#pragma mark 0260 TODO:[DONE] 宮下さんLineパラメ(LINE出ないので不要)
 	ns = setBasicSection("fp-0260-4pt", true, true);
 	setCamActorScene(ns);
 	ns->addScene(SCENE_FOURPT, false, true, false, true);
@@ -273,6 +314,12 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_FOURPT, ACTOR_SHIMAJI, JOINT_LEFT_KNEE);
 	ns->addExtractor(SCENE_FOURPT, ACTOR_SHIMAJI, JOINT_RIGHT_KNEE);
 	
+	ns->addTuneF(SCENE_FOURPT, "Twist resolution", 52.0);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension width", 3.47);
+	ns->addTuneF(SCENE_FOURPT, "Twist extension height", 1.40);
+	setActorViewing(ns, true, true, true, true);
+
+	
 #pragma mark 0280
 	ns = setBasicSection("fp-0280-upside-bigbox", true, true);
 	ns->addScene(SCENE_UPSIDE, false, true, false, true);
@@ -283,7 +330,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0290
 	ns = setBasicSection("fp-0290-3pt-bigbox", true, true);
 	setCamActorScene(ns);
@@ -297,7 +345,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_KNEE);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_LEFT_SHOULDER);
 	ns->addExtractor(SCENE_BIGBOX, ACTOR_MIYASHITA, JOINT_RIGHT_SHOULDER);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0300 TODO: [DONE]モンスター分配
 	ns = setBasicSection("fp-0300-monster", true, true);
 	setCamActorScene(ns);
@@ -308,7 +357,8 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_MONSTER, ACTOR_SHIMAJI, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER, ACTOR_MIYASHITA, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER_2, ACTOR_MIYASHITA, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0310
 	ns = setBasicSection("fp-0310-monster-burst", true, true);
 	setCamActorScene(ns);
@@ -318,12 +368,14 @@ void dpConductor::setSections()
 	ns->addExtractor(SCENE_MONSTER, ACTOR_KOJIRI, JOINT_NECK);
 	ns->addExtractor(SCENE_MONSTER, ACTOR_MIYASHITA, JOINT_NECK);
 	ns->addExtractor(SCENE_BURST, ACTOR_SHIMAJI, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 #pragma mark 0320
 	ns = setBasicSection("fp-0320-chaser", true, true);
 	setCamActorScene(ns);
 	ns->addScene(SCENE_CHASER, false, true, false, true);
 	ns->addExtractor(SCENE_CHASER, ACTOR_KOJIRI, JOINT_NECK);
 	ns->addExtractor(SCENE_CHASER, ACTOR_MIYASHITA, JOINT_NECK);
-	
+	setActorViewing(ns, true, true, true, true);
+
 }
