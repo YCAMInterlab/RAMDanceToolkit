d887d8d8da8ea6ca739136feb2d4e970a8fcef86
kezzardrix <ryokanda62@gmail.com>
2016-02-03 18:01:30 +0900

    BDisplayOutline

---
+++ DividualPlays/Jan2016_Creation/src/scenes/knd/afterBaseSceneDraw/dpBDisplayOutline.cpp
@@ -0,0 +1,9 @@
+//
+//  dpBDisplayOutline.cpp
+//  RAMDanceToolkit
+//
+//  Created by kezzardrix2 on 2016/02/03.
+//
+//
+
+#include "dpBDisplayOutline.h"
---
+++ DividualPlays/Jan2016_Creation/src/scenes/knd/afterBaseSceneDraw/dpBDisplayOutline.h
@@ -0,0 +1,14 @@
+//
+//  dpBDisplayOutline.h
+//  RAMDanceToolkit
+//
+//  Created by kezzardrix2 on 2016/02/03.
+//
+//
+
+#ifndef __RAMDanceToolkit__dpBDisplayOutline__
+#define __RAMDanceToolkit__dpBDisplayOutline__
+
+#include <stdio.h>
+
+#endif /* defined(__RAMDanceToolkit__dpBDisplayOutline__) */
