5fd3d91df4563040da9a911fb69f9d541cb57c85
Yoshito Onishi <g6136t@gmail.com>
2016-02-05 17:18:03 +0900

    Inspection

--- DividualPlays/dpScore/src/scenes/dpScoreCompoundLine.cpp
+++ DividualPlays/dpScore/src/scenes/dpScoreCompoundLine.cpp
@@ -15,10 +15,10 @@ DP_SCORE_NAMESPACE_BEGIN
 
 
 LineType::LineType(const ofVec3f& v0, const ofVec3f& v1) :
-mV0(v0),
-mV1(v1)
+	mV0(v0),
+	mV1(v1)
 {
-    
+
 }
 
 LineType::~LineType()
@@ -28,30 +28,30 @@ LineType::~LineType()
 
 void LineType::set(const ofVec3f& v0, const ofVec3f& v1)
 {
-    mV0 = v0;
-    mV1 = v1;
+	mV0 = v0;
+	mV1 = v1;
 }
 
 void LineType::draw()
 {
-    ScopedMatrix m;
-    if (compoundGetEnableCollapse()) {
-        // GPU ver
-        transformGL();
-        ofLine(mV0, mV1);
-        // CPU ver
-        //ofMatrix4x4 m;
-        //m.glTranslate(mPos);
-        //m.glRotate(mAng.z, 0.f, 0.f, 1.f);
-        //m.glRotate(mAng.y, 0.f, 1.f, 0.f);
-        //m.glRotate(mAng.x, 1.f, 0.f, 0.f);
-        //const ofVec3f v0 {mV0 * m};
-        //const ofVec3f v1 {mV1 * m};
-        //ofLine(v0, v1);
-    }
-    else {
-        ofLine(mV0, mV1);
-    }
+	ScopedMatrix m;
+	if (compoundGetEnableCollapse()) {
+		// GPU ver
+		transformGL();
+		ofLine(mV0, mV1);
+		// CPU ver
+		//ofMatrix4x4 m;
+		//m.glTranslate(mPos);
+		//m.glRotate(mAng.z, 0.f, 0.f, 1.f);
+		//m.glRotate(mAng.y, 0.f, 1.f, 0.f);
+		//m.glRotate(mAng.x, 1.f, 0.f, 0.f);
+		//const ofVec3f v0 {mV0 * m};
+		//const ofVec3f v1 {mV1 * m};
+		//ofLine(v0, v1);
+	}
+	else {
+		ofLine(mV0, mV1);
+	}
 }
 
 DP_SCORE_NAMESPACE_END
