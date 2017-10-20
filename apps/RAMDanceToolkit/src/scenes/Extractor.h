6d1d59866c5c791be08a156a6a62a0094c6769b2
Ovis aries <culminate0523@gmail.com>
2017-10-20 13:49:30 +0900

    Extractor modify

--- apps/RAMDanceToolkit/src/scenes/Extractor.h
+++ apps/RAMDanceToolkit/src/scenes/Extractor.h
@@ -30,17 +30,38 @@ public:
 	void draw()
 	{
 		rdtk::BeginCamera();
+		
+		/*  Debug preview  */
 		mEx.draw();
 		
-		if (mEx.getNumPort() >= 3)
+		for (int i = 0;i < mEx.getNumPort();i++)
 		{
-			ofVec3f p1 = mEx.getPositionAt(0);
-			ofVec3f p2 = mEx.getPositionAt(1);
-			ofVec3f p3 = mEx.getPositionAt(2);
 			
-			ofDrawLine(p1, p2);
-			ofDrawLine(p2, p3);
-			ofDrawLine(p3, p1);
+			/*   Get extractor informations */
+			 
+			ofMatrix4x4 mat = mEx.getNodeAt(i).getGlobalTransformMatrix();
+			ofVec3f pos		= mEx.getPositionAt(i);
+			ofVec3f next	= mEx.getPositionAt((i+1) % mEx.getNumPort());
+			ofVec3f vel		= mEx.getVelocityAt(i);
+			
+			
+			/*   Draw Primitives  */
+			
+			ofPushMatrix();
+			{
+				ofMultMatrix(mat);
+				
+				ofSetColor(255);
+				ofDrawBox(0, 0, 0, vel.length() * 10.0);
+				ofNoFill();
+				ofSetColor(0);
+				ofDrawBox(0, 0, 0, vel.length() * 11.0);
+				ofFill();
+			}
+			ofPopMatrix();
+			
+			ofSetColor(0, 255, 150);
+			ofDrawLine(pos, next);
 		}
 		
 		rdtk::EndCamera();
